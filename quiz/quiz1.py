85174dfa3a171399c750bbd77bf3c88018bdeade
Zeke <zzumbro@jbu.edu>
2021-02-14 13:57:22 -0600

    adding quiz solution and tests to be graded against

--- quiz/quiz1.py
+++ quiz/quiz1.py
@@ -1,2 +1,10 @@
-def first():
-    pass
+from typing import Any, List
+
+
+def first(a: List[Any], cmd: str):
+    cmd = cmd.strip().lower()
+    if not a or cmd not in ["start", "first", "end"]:
+        return None
+    if cmd == "end":
+        return next(reversed(a))
+    return next(iter(a))
