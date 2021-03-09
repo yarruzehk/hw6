f79a16a12abc476d51e7d3fb54948e406c3e5c76
Zeke <zzumbro@jbu.edu>
2021-03-08 19:39:49 -0600

    adding tests around exam1

---
+++ test/exam/test_exam1.py
@@ -0,0 +1,59 @@
+from exam.exam1 import (
+    remove_vision,
+    list_of_names,
+    filter_list_of_names,
+    return_last_match,
+)
+
+import pytest
+
+
+def test_vision_is_not_in_list():
+    assert "vision" in list_of_names
+    assert "vision" not in remove_vision()
+
+
+def test_wanda_in_list():
+    assert "wanda" in list_of_names
+    assert "wanda" in remove_vision()
+
+
+@pytest.mark.parametrize(
+    "in_str,out",
+    [
+        ("x", []),
+        ("a", ["alex", "amanda", "aaron"]),
+        ("aa", ["aaron"]),
+        ("wanda", ["wanda"]),
+        (
+            "",
+            [
+                "alex",
+                "amanda",
+                "aaron",
+                "charlie",
+                "vision",
+                "wanda",
+                "tommy",
+                "sophie",
+            ],
+        ),
+    ],
+)
+def test_filter_names(in_str, out):
+    assert filter_list_of_names(in_str) == out
+
+
+@pytest.mark.parametrize(
+    "in_str,out",
+    [
+        ("x", "alex"),
+        ("ie", "sophie"),
+        ("a", "wanda"),
+        ("lie", "charlie"),
+        ("q", None),
+    ],
+)
+def test_return_last_match(in_str, out):
+    assert return_last_match(in_str) == out
+
