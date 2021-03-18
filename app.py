9746c7a2f37732365dbc4aab3af2f9bd17f3f6c0
Zeke <zzumbro@jbu.edu>
2021-03-18 11:01:57 -0500

    in_class work

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from typing import Optional
 from fastapi import FastAPI
 
 from house.endpoints import router as house_router
+from student.endpoints import router as student_router
 from users.endpoints import router
 
 app = FastAPI()
 
 app.include_router(router, prefix="/users")
 app.include_router(house_router, prefix="/house")
+app.include_router(student_router)
 
 
 @app.get("/")
---
+++ student/model.py
@@ -0,0 +1,47 @@
+from datetime import datetime, timedelta
+
+
+class User:
+    def __init__(self, username):
+        logger.info(f"creating a new user {username}")
+        self.username: str = username
+
+    def to_upper(self):
+        return self.username.upper()
+
+    def to_lower(self):
+        return self.username.lower()
+
+
+class Student(User):
+    def __init__(self, student_id, username, *, infection_date=None):
+        logger.debug("creating new student")
+        super().__init__(username)
+        logger.debug("finished calling super")
+        self.username = username
+        self.student_id = student_id
+        self.infection_date = infection_date
+
+    def student_dictionary(self):
+        return {"student_id": self.student_id, "username": self.username}
+
+    @property
+    def status(self):
+        now = datetime.now()
+        if self.infection_date == None:
+            return "healthy"
+        if timedelta(days=14) < now - self.infection_date:
+            return "vac"
+
+
+if __name__ == "__main__":
+    from loguru import logger
+
+    u = User("username1")
+    s = Student(1, "student1")
+    s2 = Student(2, "student2")
+    logger.info(u.to_upper())
+    logger.warning(s.to_lower())
+    logger.info(s.student_dictionary())
+    logger.error(s2.student_dictionary())
+    logger.debug(s.status)
